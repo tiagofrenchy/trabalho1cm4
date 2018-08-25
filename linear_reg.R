4ce1de13432dfdab6e9d3f8451b2b0465db05cf1
Tiago Siqueira <TSiqueira@Mac1.domdmz.einstein.br>
2018-08-25 07:59:51 -0300

    final version of the script

--- linear_reg.R
+++ linear_reg.R
@@ -8,29 +8,24 @@ library(ggplot2)
 hiv <- read_excel("HIV.xlsx")
 View(hiv)
 
-##Visualizando dados para ver distribuiçãoe possíveis correlações
-# pairs.panels(hiv[,-9], 
-#              method = "pearson", # correlation method
-#              hist.col = "#650cff",
-#              density = TRUE,  # show density plots
-#              ellipses = TRUE # show correlation ellipses
-# )
-# 
-# chart.Correlation(hiv, histogram=TRUE, pch=19)
-# 
-# hist(hiv$idade, breaks = 20)
-# hist(hiv$pn, breaks = 20)
-# hist(hiv$imc, breaks = 20)
-# hist(hiv$homa, breaks = 20)
-# hist(hiv$cd4, breaks = 20)
-# hist(hiv$ct, breaks = 20)
-# 
-# plot(density(hiv$idade))
-# plot(density(hiv$pn))
-# plot(density(hiv$imc))
-# plot(density(hiv$homa))
-# plot(density(hiv$cd4))
-# plot(density(hiv$ct))
+#Visualizando dados para ver distribuiçãoe possíveis correlações
+pairs.panels(hiv[,-9], method = "pearson", hist.col = "#650cff", density = TRUE, ellipses = TRUE)
+
+chart.Correlation(hiv, histogram=TRUE, pch=19)
+
+hist(hiv$idade, breaks = 20)
+hist(hiv$pn, breaks = 20)
+hist(hiv$imc, breaks = 20)
+hist(hiv$homa, breaks = 20)
+hist(hiv$cd4, breaks = 20)
+hist(hiv$ct, breaks = 20)
+
+plot(density(hiv$idade))
+plot(density(hiv$pn))
+plot(density(hiv$imc))
+plot(density(hiv$homa))
+plot(density(hiv$cd4))
+plot(density(hiv$ct))
 
 #Parte1: Comparando os grupos para testar similaridade antes da intervenção
 hiv$sex <- gsub("0","Fem", hiv$sex)
@@ -54,17 +49,34 @@ lm <- lm(ct~imc, data=hiv)
 summary(lm)
 sum_lm <- summary(lm)
 
-ggplot(hiv, aes(x = imc, y = ct, color = ip)) +
+coeff=coefficients(lm)
+eq = paste0("y = ", round(coeff[2],1), "*x + ", round(coeff[1],1))
+
+ggplot(hiv, aes(x = imc, y = ct)) +
   geom_point(data = hiv, shape = 19) +
   geom_smooth(data = hiv, method = lm, se = FALSE, color = hiv$ip) +
-  scale_color_manual(values = c("chocolate1", "slateblue3"), name = "Tratamento", breaks = c("Sem IP", "Com IP"), labels = c("Controle", "IP"))+
   labs(title = "Correlacao entre o Colesterol Total e o IMC", x = "IMC", y = "Colesterol Total" ) +
-  geom_abline(intercept = -70.110, slope = 14.482, color="black", size=.5)
+  geom_abline(intercept = -70.110, slope = 14.482, color="black", size=.5) +
+  annotate("text", x = 19.2, y = 195, colour = "black", size = 5, label = print(eq))
 
 #Parte3: Aplicar modelo de regressão linear multipla para verificar interferencia de ip no anterior
 lm2 <- lm(ct~imc + ip, data=hiv)
 summary(lm2)
 sum_lm2 <- summary(lm2)
+
+semip <- subset(hiv, ip == "Sem IP")
+lm3 <- lm(ct~imc, data=semip)
+summary(lm3)
+sum_lm3 <- summary(lm3)
+coeff3=coefficients(lm3)
+eq3 = paste0("y = ", round(coeff3[2],1), "*x + ", round(coeff3[1],1))
+
+comip <- subset(hiv,ip == "Com IP")
+lm4 <- lm(ct~imc, data=comip)
+summary(lm4)
+sum_lm4 <- summary(lm4)
+coeff4=coefficients(lm4)
+eq4 = paste0("y = ", round(coeff4[2],1), "*x + ", round(coeff4[1],1))
 
 ggplot(hiv, aes(x = imc, y = ct, color = ip)) +
   geom_point(data = hiv, shape = 19) +
@@ -72,20 +84,17 @@ ggplot(hiv, aes(x = imc, y = ct, color = ip)) +
   scale_color_manual(values = c("chocolate1", "slateblue3"), name = "Tratamento", breaks = c("Sem IP", "Com IP"), labels = c("Controle", "IP"))+
   labs(title = "Correlacao entre o Colesterol Total e o IMC", x = "IMC", y = "Colesterol Total" ) +
   geom_abline(intercept = -26.525, slope = 11.208, color="slateblue3", size=.5) +
-  geom_abline(intercept = -53.786, slope = 14.031, color="chocolate1", size=.5)
+  annotate("text", x = 18.7, y = 170, colour = "slateblue3", size = 5, label = print(eq3)) +
+  geom_abline(intercept = -53.786, slope = 14.031, color="chocolate1", size=.5) +
+  annotate("text", x = 19.2, y = 205, colour = "chocolate1", size = 5, label = print(eq4))
 
-##experimental
+#Parte4: Identificar quais variaveis tem efeito sinergico a ip
 
-semip <- subset(hiv, ip == "Sem IP")
-lm3 <- lm(ct~imc, data=semip)
-summary(lm3)
-sum_lm3 <- summary(lm3)
+lmf1 <- lm(ct~imc + ip + homa + cd4 + pn + idade + sex, data=hiv)
+summary(lmf1)
+sum_lmf1 <- summary(lmf1)
 
-comip <- subset(hiv,ip == "Com IP")
-lm4 <- lm(ct~imc, data=comip)
-summary(lm4)
-sum_lm4 <- summary(lm4)
+lmf2 <- lm(ct~imc + ip + sex, data=hiv)
+summary(lmf2)
+sum_lmf2 <- summary(lmf2)
 
-##experimental
-
-#Parte4:
